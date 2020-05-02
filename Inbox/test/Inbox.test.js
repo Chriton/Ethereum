8eb41b8e3e1d62cef4583e9cf696d9f7bdf459d6
Doru Muntean <doru.muntean@netcentric.biz>
2020-05-02 17:22:05 +0300

    added 2 more tests

--- Inbox/test/Inbox.test.js
+++ Inbox/test/Inbox.test.js
@@ -1,42 +1,16 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
+const provider = ganache.provider();
 
 // Web3 is a constructor
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
-
-// start mocha Example
-// class Car {
-//     park() {
-//         return 'stopped';
-//     }
-//
-//     drive() {
-//         return 'vroom';
-//     }
-// }
-//
-// let car;
-//
-// beforeEach(() => {
-//     car = new Car();
-// });
-//
-// describe('Car tests', () => {
-//     it('can park', () => {
-//         assert.equal(car.park(), 'stopped');
-//     });
-//
-//     it('can drive', () => {
-//         assert.equal(car.drive(), 'vroom');
-//     })
-// });
-// end mocha Example
-
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there!'
 
 beforeEach(async () => {
     // Get a list of all the accounts
@@ -44,14 +18,40 @@ beforeEach(async () => {
 
     // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-      .deploy({ data: bytecode, arguments: ['Hi there!'] })
-      .send({ from: accounts[0], gas: '1000000' })
+      .deploy({
+        data: bytecode,
+        arguments: [INITIAL_STRING]
+      })
+      .send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+
+      //inbox.setProvider(provider);
 });
 
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
         //console.log(accounts);
-        console.log(inbox);
-    })
+        //console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+      const message = await inbox.methods.message().call();
+      assert.equal(message, INITIAL_STRING);
+    });
+
+    it('can change the message', async () => {
+      // TODO - set new message
+      await inbox.methods.setMessage('bye')
+      .send({
+        from: accounts[0],
+        gas: '1000000'
+      })
+
+      const message = await inbox.methods.message().call();
+      assert.equal(message, 'bye');
+    });
 });
